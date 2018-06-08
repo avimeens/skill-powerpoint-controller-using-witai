9d10979acce47fa3ed0eb749cebfe15245bd156a
Avinash Vyas <avinash.vyas@nokia.com>
2018-06-08 11:23:39 -0400

    Initial commit

--- __init__.py
+++ __init__.py
@@ -1,15 +1,89 @@
-from adapt.intent import IntentBuilder
+import requests
 from mycroft import MycroftSkill, intent_handler
+from mycroft.util.log import getLogger
 
 
 class PowerpointControllerUsingWitaiSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.url = "http://135.222.162.94:8001"
+        self.file_opened = False
 
-    @intent_handler(IntentBuilder().require('PowerpointControllerUsingWitai'))
-    def handle_powerpoint_controller_using_witai(self, message):
-        self.speak_dialog('powerpoint.controller.using.witai')
+    def initialize(self):
+        self.register_wit_intent('ppt.contoller.using.witai',
+        self.handle_ppt_controller_using_witai)
+        self.register_wit_intent('open.json', self.handle_ppt_open)
+        self.register_wit_intent('next.json', self.handle_next_slide)
+        self.register_wit_intent('previous.json', self.handle_prev_slide)
+        self.register_wit_intent('close.json', self.handle_ppt_close)
 
+    def handle_ppt_controller_using_witai(self, message):
+        self.speak_dialog('ppt.controller.using.witai')
+
+    def handle_ppt_open(self, message):
+        filename = message.data.get("filename")
+        if filename is None:
+            self.speak_dialog('ppt.specifyfile')
+        else:	
+            self.enclosure.mouth_text("Nova opening file " + filename)
+            self.file_opened = True;
+            # Send a rest request
+            param = {'filename':filename}
+            self.enclosure.mouth_text("Sending request to " + self.url);
+            response = requests.get(self.url, param)
+            #response = requests.codes.ok
+            resp = {'filename' : filename}
+            if response.status_code == requests.codes.ok:
+            #if response == requests.codes.ok:
+       	        self.speak_dialog('ppt.open', data=resp)
+            else: 
+                self.speak_dialog('ppt.filenotfound')
+
+    def handle_next_slide(self, message):
+        if self.file_opened: 
+            # Send a rest request
+            nurl = self.url + "/nextpage"
+            self.enclosure.mouth_text("Sending request to " + nurl);
+            response = requests.get(nurl)
+            #response = requests.codes.ok
+            if response.status_code == requests.codes.ok:
+            #if response == requests.codes.ok:
+                self.speak_dialog('ppt.next')
+            else: 
+                self.speak_dialog('ppt.filenotfound')
+        else: 
+            self.speak_dialog('ppt.filenotopen')
+	
+    def handle_prev_slide(self, message):
+        if self.file_opened: 
+            # Send a rest request
+            purl = self.url + "/prevpage"
+            self.enclosure.mouth_text("Sending request to " + purl);
+            response = requests.get(purl)
+            #response = requests.codes.ok
+            if response.status_code == requests.codes.ok:
+            #if response == requests.codes.ok:
+                self.speak_dialog('ppt.prev')
+            else: 
+                self.speak_dialog('ppt.filenotfound')
+        else: 
+            self.speak_dialog('ppt.filenotopen')
+
+    def handle_ppt_close(self, message):
+        # Send a rest request
+        if self.file_opened: 
+            purl = self.url + "/close"
+            self.enclosure.mouth_text("Sending request to " + purl);
+            response = requests.get(purl)
+            #response = requests.codes.ok
+            if response.status_code == requests.codes.ok:
+            #if response == requests.codes.ok:
+                self.file_opened = False
+                self.speak_dialog('ppt.close')
+            else: 
+                self.speak_dialog('ppt.filenotfound')
+        else: 
+            self.speak_dialog('ppt.filenotopen')
 
 def create_skill():
     return PowerpointControllerUsingWitaiSkill()
